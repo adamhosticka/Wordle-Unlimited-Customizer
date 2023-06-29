9d8fc83231037f537f6a922a77c170720c9de945
Adam Hosticka <hostiada@fit.cvut.cz>
2023-06-29 15:13:01 +0200

    dev: remember button state after popup reopen

--- js/content.js
+++ js/content.js
@@ -1,8 +1,14 @@
 const GUESSES = 6;
 const LETTERS = 5;
+const SELECTED = "selected";
+const NOT_SELECTED = "not-selected";
+
 const HIDE_TILE_CLASS_NAME = "hideTile";
 
-
+let buttonStates = {
+	"valid": false,
+	"buttons": [],
+}
 
 function getGameThemeManager() {
 	return document.getElementsByTagName("game-app").item(0).shadowRoot.children.item(1);
@@ -19,18 +25,6 @@ function getTile(rowId, colId) {
 }
 
 
-function init() {
-	for(let rowId = 0; rowId < GUESSES; rowId++) {
-		for(let colId = 0; colId < LETTERS; colId++) {
-			getGameTile(rowId, colId).shadowRoot.children.item(0).sheet.insertRule(
-				`.${HIDE_TILE_CLASS_NAME} {background: black !important;}`, 0
-			);
-		}
-	}
-}
-init();
-
-
 function hideTile(rowId, colId) {
 	getTile(rowId, colId).classList.add(HIDE_TILE_CLASS_NAME);
 }
@@ -39,30 +33,53 @@ function showTile(rowId, colId) {
 	getTile(rowId, colId).classList.remove(HIDE_TILE_CLASS_NAME);
 }
 
-function hideColumn(colId) {
+function toggleColumn(state, colId) {
 	for(let rowId = 0; rowId < GUESSES; rowId++) {
-		hideTile(rowId, colId);
+		if(state == SELECTED) {
+			hideTile(rowId, colId);
+		} else {
+			showTile(rowId, colId);
+		}
 	}
 }
 
-function showColumn(colId) {
-	for(let rowId = 0; rowId < GUESSES; rowId++) {
-		showTile(rowId, colId);
+function toggleKeyboard(state) {
+	let hidden = false;
+	if(state == SELECTED) {
+		hidden = true;
 	}
-}
-
-function toggleKeyboard(hidden) {
 	getGameThemeManager().getElementsByTagName("game-keyboard").item(0).hidden = hidden;
 }
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-	if(message.action === "hideColumn") {
-		hideColumn(message.colId);	
-  	}
-	else if(message.action == "showColumn") {
-		showColumn(message.colId);
+	if(message.action == "getButtonStates") {
+		sendResponse({valid: buttonStates.valid, buttons: buttonStates.buttons});
+		return;
 	}
+
+	if(message.buttons) {
+		buttonStates.valid = true;
+		buttonStates.buttons = message.buttons
+	}
+	let buttonId = message.buttonId;
+	let state = buttonStates.buttons[buttonId].state;
+
+	if(message.action === "toggleColumn") {
+		toggleColumn(state, message.colId);	
+  	}
 	else if(message.action == "toggleKeyboard") {
-		toggleKeyboard(message.hidden);
+		toggleKeyboard(state);
 	}
 });
+
+
+function init() {
+	for(let rowId = 0; rowId < GUESSES; rowId++) {
+		for(let colId = 0; colId < LETTERS; colId++) {
+			getGameTile(rowId, colId).shadowRoot.children.item(0).sheet.insertRule(
+				`.${HIDE_TILE_CLASS_NAME} {background: black !important; color: white !important;}`, 0
+			);
+		}
+	}
+}
+init();
--- js/extension.js
+++ js/extension.js
@@ -2,8 +2,38 @@ const COLUMNS = 5;
 const SELECTED = "selected";
 const NOT_SELECTED = "not-selected";
 
-function sendMessageToContentScript(message) {
+
+let buttons = [];
+
+class Button {
+  constructor(parent, message, additionalClasses, innerHTMLNotSelected, innerHTMLSelected, state = NOT_SELECTED) {
+    this.id = buttons.length;
+    this.element = document.createElement("button");
+    this.parent = parent;
+    this.message = message;
+    this.additionalClasses = additionalClasses;
+    this.innerHTMLNotSelected = innerHTMLNotSelected;
+    this.innerHTMLSelected = innerHTMLSelected;
+    this.state = state;
+  }
+}
+
+
+function getButtonStates() {
+  return new Promise((resolve, reject) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      chrome.tabs.sendMessage(tabs[0].id, {action: "getButtonStates"}, function (response) {
+        resolve(response);
+      });
+    });
+  });
+}
+
+
+function sendMessageToContentScript(message, buttonId) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    message.buttons = buttons.map(({state}) => ({state}));
+    message.buttonId = buttonId;
     chrome.tabs.sendMessage(tabs[0].id, message);
   });
 }
@@ -12,49 +42,69 @@ function sendMessageToContentScript(message) {
 function initHideColumnsContainer() {
   let hideColumnsContainer = document.getElementById("hide-columns-buttons-container");
   for(let colId = 0; colId < COLUMNS; colId++) {
-    let button = document.createElement("button");
-    button.innerHTML = colId + 1;
-    button.classList.add("me-2", "not-selected");
-    button.value = "not-selected";
-    hideColumnsContainer.appendChild(button);
-    button.addEventListener("click", function (e) {
-      if(e.target.value == NOT_SELECTED) {
-        e.target.value = SELECTED;
-        e.target.classList.remove(NOT_SELECTED);
-        e.target.classList.add(SELECTED);
-        sendMessageToContentScript({ action: "hideColumn", colId: colId });
-      } else if(e.target.value == SELECTED) {
-        e.target.value = NOT_SELECTED;
-        e.target.classList.remove(SELECTED);
-        e.target.classList.add(NOT_SELECTED);
-        sendMessageToContentScript({ action: "showColumn", colId: colId });
-      }
-    });
+    let button = new Button(hideColumnsContainer, {action: "toggleColumn", colId: colId}, ["me-2"], colId + 1, colId + 1);
+    buttons.push(button);
   }
 }
 
 function initHideKeyboardButton() {
-  let hideKeyboardButton = document.getElementById("hideKeyboard");
-  hideKeyboardButton.addEventListener("click", function (e) {
-    if(e.target.value == NOT_SELECTED) {
-      e.target.value = SELECTED;
-      e.target.innerHTML = "Show keyboard";
-      e.target.classList.remove(NOT_SELECTED);
-      e.target.classList.add(SELECTED);
-      sendMessageToContentScript({ action: "toggleKeyboard", hidden: true });
-    } else if(e.target.value == SELECTED) {
-      e.target.value = NOT_SELECTED;
-      e.target.innerHTML = "Hide keyboard";
-      e.target.classList.remove(SELECTED);
-      e.target.classList.add(NOT_SELECTED);
-      sendMessageToContentScript({ action: "toggleKeyboard", hidden: false });
-    }
-  });
+  let keyboardControlsContainer = document.getElementById("keyboard-controls-container");
+  let hideKeyboardButton = new Button(keyboardControlsContainer, {action: "toggleKeyboard"}, [], "Hide keyboard", "Show keyboard");
+  buttons.push(hideKeyboardButton); 
 }
 
-function init() {
+
+
+function switchButtonState(button, currentState) {
+  if(currentState == NOT_SELECTED) {
+    button.state = SELECTED;
+  } else {
+    button.state = NOT_SELECTED;
+  }
+}
+
+function changeButtonAttributes(button) {
+  if(button.state == SELECTED) {
+    button.element.innerHTML = button.innerHTMLSelected;
+    button.element.classList.add(SELECTED);
+    button.element.classList.remove(NOT_SELECTED);
+  } else if(button.state == NOT_SELECTED) {
+    button.element.innerHTML = button.innerHTMLNotSelected;
+    button.element.classList.add(NOT_SELECTED);
+    button.element.classList.remove(SELECTED);
+  }
+  button.element.value = button.state;
+}
+
+function createButtonsAndSetListeners() {
+  for(let button of buttons) {
+    changeButtonAttributes(button);
+
+    for(const className of button.additionalClasses) {
+      button.element.classList.add(className);
+    }
+    button.parent.appendChild(button.element);
+  
+    button.element.addEventListener("click", function(e) {
+      switchButtonState(button, e.target.value);
+      changeButtonAttributes(button);
+      sendMessageToContentScript(button.message, button.id);
+    });
+  }
+}
+
+async function init() {
   initHideColumnsContainer();
   initHideKeyboardButton();
+  
+  const buttonStates = await getButtonStates();
+  if(buttonStates.valid) {
+    for(let buttonId = 0; buttonId < buttonStates.buttons.length; buttonId++) {
+      buttons[buttonId].state = buttonStates.buttons[buttonId].state;
+      changeButtonAttributes(buttons[buttonId]);
+    }
+  }
+  
+  createButtonsAndSetListeners();
 }
 init();
-
